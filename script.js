f9b5bdeac13aa5d9df45cd32250dc7d30b9e3011
ZehnoHome <126801501+Zehnooo@users.noreply.github.com>
2025-07-05 19:30:31 -0500

    attempting random color generation

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const grid = document.querySelector(".grid-container");
 
 for (i = 0; i < 16 * 16; i++) {
-  const div = document.createElement("div");
-  div.classList.add("tile");
-  grid.appendChild(div);
+  const tile = document.createElement("div");
+  tile.classList.add("tile");
+  grid.appendChild(tile);
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  tile.addEventListener("click", () => {
+    tile.style.backgroundColor = randomColor;
+  });
 }
